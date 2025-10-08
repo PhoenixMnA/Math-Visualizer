c4931ecda808fd4649081797261b130778f44c90
PhoenixMnA <fstorm797@gmail.com>
2025-10-08 10:13:47 +0000

    mobile tweaks

--- resources/views/math/index.blade.php
+++ resources/views/math/index.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
   <title>Math Visualizer</title>
 <!-- ✅ Plotly (latest v3 release) -->
 <script src="https://cdn.plot.ly/plotly-3.1.0.min.js"></script>
